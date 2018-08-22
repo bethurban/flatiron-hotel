77f3b3a7a161879532eacef43abcd40d04bc3126
bethurban <beth.urban@gmail.com>
2018-08-22 15:01:53 -0400

    Add new, create, destroy to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,21 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    @guest = Guest.find_by(email: params[:email])
+    if @guest && @guest.authenticate(params[:password])
+      session[:user_id] = @guest.id
+      redirect_to guest_path(@guest)
+    else
+      redirect_to signin_path
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
+
 end
