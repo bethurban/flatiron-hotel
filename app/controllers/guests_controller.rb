c004e8bce5d06ba14365f7d32a5a6dbda205a3bb
bethurban <beth.urban@gmail.com>
2018-08-22 16:35:26 -0400

    Create basic form at guests#new; add new, create, and guest_params to GuestsController

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,3 +1,23 @@
 class GuestsController < ApplicationController
-  
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.new(guest_params)
+    if @guest.save
+      session[:user_id] = @guest.id
+      redirect_to guest_path(@guest)
+    else
+      redirect_to new_guest_path
+    end
+  end
+
+  private
+
+  def guest_params
+    params.require(:guest).permit(:first_name, :last_name, :email, :phone_number, :password)
+  end
+
 end
