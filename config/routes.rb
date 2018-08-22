a83bbfe225381615dd30a33f9bac9153354e44d6
bethurban <beth.urban@gmail.com>
2018-08-22 14:52:53 -0400

    Create Sessions and Welcome controllers, view folders, welcome#home erb, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
   resources :bookings
   resources :rooms
   resources :guests
