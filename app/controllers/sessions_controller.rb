29f2dbd99e86a3f8af19efc26a92ef950664a204
bethurban <beth.urban@gmail.com>
2018-08-22 17:08:26 -0400

    Added pry to Gemfile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @guest = Guest.find_by(email: params[:email])
-    if @guest && @guest.authenticate(params[:password])
+    @guest = Guest.find_by(email: params[:guest][:email])
+    if @guest && @guest.authenticate(params[:guest][:password])
       session[:user_id] = @guest.id
       redirect_to guest_path(@guest)
     else
