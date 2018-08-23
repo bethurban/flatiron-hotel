7be0abac2fb1a0ed8927f642f2034426c5521863
bethurban <beth.urban@gmail.com>
2018-08-23 17:13:54 -0400

    Fill out RoomsController with new, create, show, and room_params. Create rooms#new and rooms#show erbs.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,3 +1,26 @@
 class RoomsController < ApplicationController
-  
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      redirect_to new_room_path
+    end
+  end
+
+  def show
+    @room = Room.find_by(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:room).permit(:room_number, :cost, :capacity)
+  end
+
 end
