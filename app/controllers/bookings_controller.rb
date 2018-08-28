4ab6e54106c93d4e4838f032e33530715fbe3e56
bethurban <beth.urban@gmail.com>
2018-08-28 18:58:59 -0400

    Continue debugging date range not saving correctly - saves as string and range can't be saved once string is converted

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,12 +14,11 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    binding.pry
-    array = @booking.date_range.split("..")
+    array = booking_params[:date_range].split("..")
     date_array1 = array.first.split("-")
-    date1 = Date.new date_array1[0], date_array1[1], date_array1[2]
+    date1 = Date.new date_array1[0].to_i, date_array1[1].to_i, date_array1[2].to_i
     date_array2 = array.last.split("-")
-    date2 = Date.new date_array2[0], date_array2[1], date_array2[2]
+    date2 = Date.new date_array2[0].to_i, date_array2[1].to_i, date_array2[2].to_i
     @booking.date_range = Range.new(date1, date2)
     if @booking.save
       redirect_to booking_path(@booking)
