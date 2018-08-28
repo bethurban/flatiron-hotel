3eb497b72f89f9f9a38ee89a05a57e5d1eefc0a6
bethurban <beth.urban@gmail.com>
2018-08-28 11:13:33 -0400

    Add link for guests to create a new booking, create bookings#new and bookings#show, add new and create paths in BookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,15 @@
 class BookingsController < ApplicationController
-  
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      redirect to new_booking_path
+    end
+  end
 end
