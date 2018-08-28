30cdf1df2ce25f8bc21f0029d368ae0c5e30a70c
bethurban <beth.urban@gmail.com>
2018-08-28 10:35:38 -0400

    Add validations to User for first/last name, email presence and format, admin needs @flatironhotel.com email address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.valid?
+      @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to new_user_path
+      render new_user_path
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :rooms, through: :bookings
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /@/
+  with_options if: :admin? do |admin|
+    admin.validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@flatironhotel\.com\z/,
+                  message: "must be a flatironhotel.com account" }
+  end
 end
