982bb9435f0b7d5d215ff20186fd971efa563f03
bethurban <beth.urban@gmail.com>
2018-08-28 10:54:10 -0400

    Add password confirmation to users#new form, add password confirmation validation and phone number validations to User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password, :admin)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password, :password_confirmation, :admin)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates_format_of :phone_number, with: /\A[1-9]\d{2}-\d{3}-\d{4}/
   validates :email, presence: true, uniqueness: true
-  validates_format_of :email, :with => /@/
+  validates_format_of :email, with: /@/
   with_options if: :admin? do |admin|
     admin.validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@flatironhotel\.com\z/,
                   message: "must be a flatironhotel.com account" }
   end
+  validates :password, confirmation: true
 end
