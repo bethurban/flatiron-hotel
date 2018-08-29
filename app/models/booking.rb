b33c71464cce9e06fc0edb6464e15273bea9faa8
bethurban <beth.urban@gmail.com>
2018-08-29 15:20:06 -0400

    Create self.bookings_between and self.available_rooms class methods for Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,4 +8,28 @@ class Booking < ApplicationRecord
     Range.new(self.checkin, self.checkout)
   end
 
+  def self.bookings_between(start_date, end_date)
+    booked = []
+    range = Range.new(start_date, end_date)
+    Booking.all.each do |booking|
+      if booking.date_range.overlaps?(range)
+        booked << booking
+      end
+    end
+    booked
+  end
+
+  def self.available_rooms(start_date, end_date, group_size)
+    available = []
+    range = Range.new(start_date, end_date)
+    Room.all.each do |room|
+      room.bookings.each do |booking|
+        if !booking.date_range.overlaps?(range) && room.capacity >= group_size && !available.include?(room)
+          available << room
+        end
+      end
+    end
+    available
+  end
+
 end
