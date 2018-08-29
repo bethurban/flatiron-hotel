25ccd99b041d2927e4eb29c6aef1fa8a4f992074
bethurban <beth.urban@gmail.com>
2018-08-29 09:47:03 -0400

    Add serialization of date_range to Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :user
   validates :date_range, presence: true
+  serialize :date_range
   validates :group_size, presence: true, numericality: { only_integer: true }
   validates :user_id, presence: true, allow_nil: true
   validates_presence_of :room_id, allow_nil: true
