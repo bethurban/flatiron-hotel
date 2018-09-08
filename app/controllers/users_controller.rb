61f5c5df31e7e274bc1a040ce9bd38a5813b9bbf
bethurban <beth.urban@gmail.com>
2018-09-08 14:20:45 -0400

    Reformat login/create account links on welcome#home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :phone_number, :password, :password_confirmation, :uid, :image)
+    params.require(:user).permit(:name, :email, :admin, :phone_number, :password, :password_confirmation, :uid, :image)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   has_many :rooms, through: :bookings
   has_secure_password
   validates :name, presence: true
-  validates_format_of :phone_number, with: /\A[1-9]\d{2}-\d{3}-\d{4}/, message: "format must be XXX-XXX-XXXX"
+  validates_format_of :phone_number, with: /\A[1-9]\d{2}-\d{3}-\d{4}/, message: "must be valid - format: XXX-XXX-XXXX"
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /@/
   with_options if: :admin? do |admin|
     admin.validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@flatironhotel\.com\z/,
-                  message: "must be a flatironhotel.com account" }
+                  message: "must be a @flatironhotel.com account if you are a hotel manager" }
   end
   validates :password, confirmation: true
 end
