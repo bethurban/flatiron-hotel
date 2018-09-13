6bb359102a61c9454f51b9c7b8ba71028ce05263
bethurban <beth.urban@gmail.com>
2018-09-13 15:32:35 -0400

    Update fonts in custom stylesheet for body and headers. Fix root path to redirect to user's show page if logged in.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
 
   def home
+    if session[:user_id]
+      @user = User.find_by_id(session[:user_id])
+      redirect_to user_path(@user)
+    end
   end
 
 end
