991449d28975a4666e00fedd278cb2bae1dd659c
bethurban <beth.urban@gmail.com>
2018-09-14 17:31:11 -0400

    Add error messaging if admin is searching for available rooms without entering a group size

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -18,11 +18,17 @@ class RoomsController < ApplicationController
       @start = Date.new params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i
       @ending = Date.new params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i
       @user = User.find_by_id(session[:user_id])
-      if @start >= Date.today && @start < @ending
-        @group_size = params[:guests].to_i
+      @group_size = params[:guests].to_i
+      if @start >= Date.today && @start < @ending && @group_size > 0
         @rooms = Room.available_rooms(@start, @ending, @group_size)
-      else
+      elsif @group_size > 0
         flash[:alert] = "Check-in and check-out dates must be in the future."
+        redirect_to user_path(@user)
+      elsif @start >= Date.today && @start < @ending
+        flash[:alert] = "Please enter the number of guests (1, 2, etc.)."
+        redirect_to user_path(@user)
+      else
+        flash[:alert] = "Check-in and check-out dates must be in the future, and the number of guests must be entered (1, 2, etc.)."
         redirect_to user_path(@user)
       end
     else
