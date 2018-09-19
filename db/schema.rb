ac80f7ae04d2dd7f8fb2abac7706029e25a7772e
bethurban <beth.urban@gmail.com>
2018-09-19 13:24:26 -0400

    Add picture column to rooms table. Add upload field to form on rooms#new.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_08_172241) do
+ActiveRecord::Schema.define(version: 2018_09_19_171638) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "room_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_09_08_172241) do
     t.integer "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
