39c721c822dd1d848217557c4c04e16007b573fd
bethurban <beth.urban@gmail.com>
2019-03-14 15:58:59 -0400

    Delete problematic migration that caused Postgres to throw error.

--- db/migrate/20180828185139_change_checkin_checkout_to_string_in_bookings.rb
+++
@@ -1,6 +0,0 @@
-class ChangeCheckinCheckoutToStringInBookings < ActiveRecord::Migration[5.2]
-  def change
-    change_column :bookings, :checkin, :string
-    change_column :bookings, :checkout, :string
-  end
-end
