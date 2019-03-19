a0b907eddac624bdaf081d290c6223abd075b7f3
bethurban <beth.urban@gmail.com>
2019-03-19 12:08:17 -0400

    Add jQuery gem. Add class to 'All Rooms' link in _header.html.erb. Begin filling out rooms.js - loading all rooms JSON data in console when user clicks on 'All Rooms.'

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   console.log('rooms.js has loaded');
+  listenForClick();
 });
+
+function listenForClick() {
+  $('.all_rooms').on('click', function(event) {
+    event.preventDefault();
+    getRooms();
+  })
+};
+
+function getRooms() {
+  $.ajax({
+    url: 'https://localhost:3000/rooms',
+    method: 'get',
+    dataType: 'json'
+  }).done(function(data) {
+
+    console.log("Data: ", data);
+  });
+};
