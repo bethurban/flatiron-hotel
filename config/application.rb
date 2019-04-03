67095bb5f62c932473f6c26a147e95e9523c8a8a
bethurban <beth.urban@gmail.com>
2019-04-02 21:55:36 -0400

    Add 'config.serve_static_assets = true' to application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module FlatironHotel
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.force_ssl = true
+    config.serve_static_assets = true
   end
 end
